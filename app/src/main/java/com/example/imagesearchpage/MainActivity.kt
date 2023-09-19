6793b2173314c98cf7632e2147370aa96788421a
yeonjooRyu <yeonjoo3751@gmail.com>
2023-09-19 22:47:49 +0900

    fragment 연결

--- app/src/main/java/com/example/imagesearchpage/MainActivity.kt
+++ app/src/main/java/com/example/imagesearchpage/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.imagesearchpage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils.replace
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import com.example.imagesearchpage.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.apply {
+            btnHome.setOnClickListener {
+                setFragment(SearchFragment())
+            }
+            btnInbox.setOnClickListener {
+                setFragment(InboxFragment())
+            }
+        }
+        setFragment(SearchFragment())
+    }
+
+    private fun setFragment(frag: Fragment) {
+        supportFragmentManager.commit {
+            replace(R.id.frameLayout, frag)
+            setReorderingAllowed(true)
+            addToBackStack("")
+        }
     }
 }
