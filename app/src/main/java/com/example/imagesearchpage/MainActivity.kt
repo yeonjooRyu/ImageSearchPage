243aff3369feecbc8e3637b0727787cbae8e381f
yeonjooRyu <yeonjoo3751@gmail.com>
2023-09-20 20:25:25 +0900

    마지막 검색어 저장 기능 구현(getSharedPreference)

--- app/src/main/java/com/example/imagesearchpage/MainActivity.kt
+++ app/src/main/java/com/example/imagesearchpage/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.imagesearchpage
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils.replace
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import com.example.imagesearchpage.databinding.ActivityMainBinding
+import com.example.imagesearchpage.databinding.FragmentSearchBinding
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -22,6 +25,8 @@ class MainActivity : AppCompatActivity() {
             }
         }
         setFragment(SearchFragment())
+
+
     }
 
     private fun setFragment(frag: Fragment) {
@@ -31,4 +36,6 @@ class MainActivity : AppCompatActivity() {
             addToBackStack("")
         }
     }
+
+
 }
--- app/src/main/java/com/example/imagesearchpage/SearchFragment.kt
+++ app/src/main/java/com/example/imagesearchpage/SearchFragment.kt
@@ -1,10 +1,14 @@
 package com.example.imagesearchpage
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil.setContentView
+import com.example.imagesearchpage.databinding.FragmentSearchBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,6 +21,7 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class SearchFragment : Fragment() {
+    private lateinit var searchBinding: FragmentSearchBinding
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +39,14 @@ class SearchFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        searchBinding = FragmentSearchBinding.inflate(inflater,container,false)
+
+        searchBinding.btnSearch.setOnClickListener{
+            saveData()
+            Toast.makeText(requireContext(), "Image Search!", Toast.LENGTH_SHORT).show()
+        }
+        loadData()
+        return searchBinding.root
     }
 
     companion object {
@@ -55,5 +67,17 @@ class SearchFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    private fun saveData(){
+        val pref = requireContext().getSharedPreferences("pref", Activity.MODE_PRIVATE)
+        val edit = pref.edit()
+        edit.putString("name", searchBinding.etSearch.text.toString())
+        edit.apply()
+    }
+
+    private fun loadData(){
+        val pref = requireContext().getSharedPreferences("pref", Activity.MODE_PRIVATE)
+        searchBinding.etSearch.setText(pref.getString("name", ""))
     }
 }
